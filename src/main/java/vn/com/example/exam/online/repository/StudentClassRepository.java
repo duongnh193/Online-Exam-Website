19e6c28a2c9d2352f2c7c37c96e5c485241860c8
haptm <phamthanhmanhha03@gmail.com>
2025-04-07 12:33:56 +0700

    update api auth and class controller

--- src/main/java/vn/com/example/exam/online/repository/StudentClassRepository.java
+++ src/main/java/vn/com/example/exam/online/repository/StudentClassRepository.java
@@ -1,7 +1,9 @@
 package vn.com.example.exam.online.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import vn.com.example.exam.online.model.entity.StudentClass;
 
+@Repository
 public interface StudentClassRepository extends JpaRepository<StudentClass, String> {
 }
---
+++ src/main/java/vn/com/example/exam/online/service/StudentClassService.java
@@ -0,0 +1,4 @@
+package vn.com.example.exam.online.service;
+
+public class StudentClassService {
+}
