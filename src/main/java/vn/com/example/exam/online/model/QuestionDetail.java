cc74c7790c758e541c8edb1810b178c185d88326
haptm <phamthanhmanhha03@gmail.com>
2025-06-14 19:27:27 +0700

    review exam

--- src/main/java/vn/com/example/exam/online/model/QuestionDetail.java
+++ src/main/java/vn/com/example/exam/online/model/QuestionDetail.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.List;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public record QuestionDetail(
         Long questionId,
         String title,
